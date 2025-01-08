e37be81c44cbb4862bd0a9324666b1f6c9328d26
Naimatullah <naimymaster@gmail.com>
2025-01-09 01:53:07 +0500

    Advantage Section Responsive Complete

--- src/Components/AdvantageSection/AdvantageSection.jsx
+++ src/Components/AdvantageSection/AdvantageSection.jsx
@@ -43,7 +43,7 @@ export const AdvantageSection = () => {
 
   return (
     <div className='advantage-section'>
-        <div className='main-container features'> 
+        <div className='features main-container'> 
             <div className='heading-container'>
                 <h2>The <span>AcceptedX</span> Advantage</h2>
                 <p>At LogoLife, we specialize in preparing students for top tech programs. Our founders have 15+ years of experience working in tech teams from Fortune 500 companies to venture-backed startups. We know exactly what it takes to break into tech as a software engineer, product designer, or product manager. We know how you can become the candidate who universities will beg to join their tech programs.</p>
