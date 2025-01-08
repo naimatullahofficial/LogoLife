3f300c49881b7523aef5bd58af8bb1bd25583b4e
Naimatullah <naimymaster@gmail.com>
2025-01-09 02:32:04 +0500

    90% project is complete

--- src/Components/ExpertsCarousel/ExpertsCarousel.jsx
+++ src/Components/ExpertsCarousel/ExpertsCarousel.jsx
@@ -49,10 +49,11 @@ export const ExpertsCarousel = () => {
     slidesToScroll: 1,
     initialSlide: 0,
     responsive: [
+
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 2,
           slidesToScroll: 2,
           infinite: true,
           dots: false,
--- src/Components/ExpertsSection/ExpertsSection.jsx
+++ src/Components/ExpertsSection/ExpertsSection.jsx
@@ -1,35 +1,42 @@
-import React from 'react'
-import "./ExpertsSection.css"
-import ExpertsCarousel from '../ExpertsCarousel/ExpertsCarousel'
-import universties_list_desktop from "../../assets/universties-list.svg"
-
-
-
+import React, { useState, useEffect } from 'react';
+import "./ExpertsSection.css";
+import ExpertsCarousel from '../ExpertsCarousel/ExpertsCarousel';
+import universties_list_desktop from "../../assets/universties-list.svg";
+import university_responsive from "../../assets/university-mobile.svg";
 
 export const ExpertsSection = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className='experts-section'>
-        <div className='main-container experts'> 
-            <div className='experts-heading-container'>
-                <p className='subtitle'>The AcceptedX Multiplier</p>
-                <h2><span className='facts'>7-12x </span>Chances of Admission By Working <br/>With the <span className='experts'> Experts </span></h2>
-        
-            </div>
-        
+      <div className='main-container experts'> 
+        <div className='experts-heading-container'>
+          <p className='subtitle'>The AcceptedX Multiplier</p>
+          <h2>
+            <span className='facts'>7-12x </span>
+            Chances of Admission By Working <br />
+            With the <span className='experts'> Experts </span>
+          </h2>
         </div>
+      </div>
 
-        <div>
-            <ExpertsCarousel />
-        </div>
+      <div>
+        <ExpertsCarousel />
+      </div>
 
-        <div className='universties-list'>
-            <img src={universties_list_desktop}/>
-        </div>
-        
-    
+      <div className='universties-list'>
+        <img src={isMobile ? university_responsive : universties_list_desktop} alt="Universities List" />
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
 export default ExpertsSection;
