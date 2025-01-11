4de904634fc2f6ae18bce2f9bad3868b7eaa146f
Naimatullah <naimymaster@gmail.com>
2025-01-12 04:45:43 +0500

    testing

--- src/Components/LogoCarousel/LogoCarousel.jsx
+++ src/Components/LogoCarousel/LogoCarousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from "react";
 import Slider from "react-slick";
 import harvard from "../../assets/harvard.png"
 import dartmouth from "../../assets/dartmouth.png"
@@ -13,43 +13,44 @@ import "./LogoCarousel.css"
 
 
 export const LogoCarousel = () => {
-    var settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 7,
-        slidesToScroll: 2,
-        initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 5,
-              slidesToScroll: 2,
-              infinite: true,
-              dots: false
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      };
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
       return (
         <div className="slider-container">
-          <div className='slider-container'>
           <Slider {...settings}>
             <div className='logo-carousel-image'>
               <img src={harvard}/>
@@ -78,7 +79,6 @@ export const LogoCarousel = () => {
             
           </Slider>
           </div>
-        </div>
       );
     }
 
