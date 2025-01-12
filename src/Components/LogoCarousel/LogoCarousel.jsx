419dd87f31836f2f8967d51cc4fcfc35c7061ff5
Naimatullah <naimymaster@gmail.com>
2025-01-12 05:34:30 +0500

    testing

--- src/Components/LogoCarousel/LogoCarousel.jsx
+++ src/Components/LogoCarousel/LogoCarousel.jsx
@@ -20,34 +20,8 @@ export const LogoCarousel = () => {
     slidesToScroll: 1,
     autoplay: true,
     speed: 2000,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 0,
     cssEase: "linear",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 6,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1
-        }
-      }
-    ]
   };
       return (
         <div className="slider-container">
